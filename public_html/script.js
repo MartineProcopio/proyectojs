0f3bb9870f66dfca5768222ff0d934e70965bf32
Martin <martineprocopio2020@gmail.com>
2022-10-16 20:07:04 -0300

    Se agrega el proyecto

---
+++ public_html/script.js
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+const message = 'Hola Mundo'; // Try edit me
+
+// Update header text
+document.write(message);
+
+// Log to console
+console.log(message);
